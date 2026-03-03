9222b3fef9e63bdd8e790d78158b12ca3e9cba6e
GabrieleES <gabriele@elettrosisteminet.it>
2026-03-03 08:47:30 +0100

    corretta istanze manuali "label" e tolti campi not utilizzati sulla main

---
+++ ProjectFiles/NetSolution/IO_Setup1.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.NativeUI;
+using FTOptix.WebUI;
+using FTOptix.CoreBase;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Modbus;
+using FTOptix.System;
+using FTOptix.Retentivity;
+using FTOptix.Alarm;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+#endregion
+
+public class IO_Setup1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
